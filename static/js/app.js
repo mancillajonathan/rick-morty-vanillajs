090744edd62014d29beb6488b408aff4b8bb3957
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-06-21 02:11:04 -0700

    Grid done

--- static/js/app.js
+++ static/js/app.js
@@ -1,39 +1,44 @@
-import API from './api.js';
-const api = new API();
+import API from './api.js'
+const api = new API()
 
-
-const $characterContainer = document.querySelector('.character');
+const $characterContainer = document.querySelector('#character-container')
+// const characterTemplate = `
+//   <article class="character">
+//     <div class="character-grid">
+//       <h2>Rick Sanchez</h2>
+//       <img src="" alt="">
+//     </div>
+//   </article>
+// `
 
 class Character {
-    constructor({name, image}) {
-        this.name = name;
-        this.image = image;
-        this.render(); // Al hacer el constructor de Character y agregar this.render automaticamente hará la función
-    }
-
-    build() {
-        return `
-        <article class="character">
-            <div class="character-grid">
-                <h2>${this.name}</h2>
-                <img src="${this.image}" alt="img">
-            </div>
-        </article>
-        `;
-    }
-
-    render() {
-        $characterContainer.innerHTML = this.build();
-    }
+  constructor({ name, image }) {
+    this.name = name
+    this.image = image
+    this.render()
+  }
+  build() {
+    return `
+      <article class="character">
+        <div class="character-grid">
+          <h2>${this.name}</h2>
+          <img src=${this.image} alt="">
+        </div>
+      </article>
+    `
+  }
+  render() {
+    $characterContainer.innerHTML = this.build()
+  }
 }
-
 
 
 async function initApp(initCharacterId) {
-    const characterData = await api.getCharacter(initCharacterId)
-    console.log(characterData);
-    
-    // const rick = new Character(characterData);
+  const characterData = await api.getCharacter(initCharacterId)
+  console.log(characterData)
+  // const rick = new Character(characterData)
 }
 
-initApp(1);
+initApp(1)
+// rick.render()
+// console.log(api.getCharacter(2))
