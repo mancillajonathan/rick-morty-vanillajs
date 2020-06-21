aa949e32658dba9a5c4341e03c18009c8cb1b9a0
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-06-21 13:51:15 -0700

    Design done

--- static/js/app.js
+++ static/js/app.js
@@ -1,44 +1,19 @@
-import API from './api.js'
-const api = new API()
+import API from './api.js';
+import Character from './character.js';
 
-const $characterContainer = document.querySelector('#character-container')
-// const characterTemplate = `
-//   <article class="character">
-//     <div class="character-grid">
-//       <h2>Rick Sanchez</h2>
-//       <img src="" alt="">
-//     </div>
-//   </article>
-// `
+const api = new API();
+let currentCharacter = 1;
+const $loadNext = document.querySelector('#load-next');
 
-class Character {
-  constructor({ name, image }) {
-    this.name = name
-    this.image = image
-    this.render()
-  }
-  build() {
-    return `
-      <article class="character">
-        <div class="character-grid">
-          <h2>${this.name}</h2>
-          <img src=${this.image} alt="">
-        </div>
-      </article>
-    `
-  }
-  render() {
-    $characterContainer.innerHTML = this.build()
-  }
-}
-
+$loadNext.addEventListener('click', async () => {
+    const characterData = await api.getCharacter(++currentCharacter);
+    new Character(characterData);
+})
 
 async function initApp(initCharacterId) {
-  const characterData = await api.getCharacter(initCharacterId)
-  console.log(characterData)
-  // const rick = new Character(characterData)
+  const characterData = await api.getCharacter(initCharacterId);
+  console.log(characterData);
+  new Character(characterData);
 }
 
-initApp(1)
-// rick.render()
-// console.log(api.getCharacter(2))
+initApp(currentCharacter);
